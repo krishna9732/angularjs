d6b265224c2c94f2461844f9f990af708aeac05d
krishna9732 <krishnapadajana1@gmail.com>
2020-03-07 22:22:27 +0530

    my-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input,HostListener, Inject} from '@angular/core';
+import {MyserviceService} from './myservice.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  constructor(@Inject(MyserviceService)myserviceService){
+    console.log(myserviceService);
+    console.log("hi, this is a component");
+  }
+  @HostListener('click',['$event'])
+  mousehover(event:Event)
+  {
+    alert("hello");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExcelComponent } from './excel/excel.component';
+import {MyserviceService} from './myservice.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExcelComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MyserviceService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    console.log("hello, this is a module");
+  }
+ }
